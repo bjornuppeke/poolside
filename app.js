7ed0317122e469cd4ca5a1d9b9c423bba6398542
Bjorn Uppeke <bjorn.uppeke@viaplay.com>
2013-12-08 01:24:32 +0100

    bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require('express'),
+	exphbs = require('express3-handlebars'),
+
 	app = express(),
 	controller = require('lib/controller')();
 
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'handlebars');
+	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+	app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function (req, res) {
 	controller.index(req, res);
+});
+
+app.post('/workout', function (req, res) {
+	controller.workout(req, res);
 });
 
 app.listen(3000, function() {
