0a9f2400a2810079c897b657f58dd7fc8f20ab84
Bjorn Uppeke <bjorn.uppeke@viaplay.com>
2013-12-10 21:57:13 +0100

    dynamic port

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.post('/workout', function (req, res) {
 	controller.workout(req, res);
 });
 
-app.listen(3000, function() {
-	console.log('listening on port 3000');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
 });
